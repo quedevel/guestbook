03dba796d3e0213ec6251ae6914a918167f5a434
quedevel <kdh90817@naver.com>
2021-01-24 02:37:23 +0900

    등록 화면 및 등록 로직 추가

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.guestbook.dto.GuestbookDTO;
@@ -30,13 +31,18 @@ public class GuestbookController {
         model.addAttribute("result", guestbookService.getList(pageRequestDTO));
     }
 
-    @GetMapping("register")
+    @GetMapping("/register")
     public void register(){
         log.info("register get...");
     }
 
+    @PostMapping("/register")
     public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
         log.info("dto..."+ dto);
+
+        Long gno = guestbookService.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+
         return "redirect:/guestbook/list";
     }
 }
